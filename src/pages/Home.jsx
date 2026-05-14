22f098bf7b067282643a83f99d5067f9fbbf79cf
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-05-14 11:40:33 +0300

    Remove initial welcome information on home page and replaced it with updated information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { Link } from "react-router-dom"
+
 function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        
+    <div className="home">
+        <div className="welcome-section">
+            <h1>Begin working out now!</h1>
             <p>Welcome to fitness flow app. Here you can find the best fitness plans and workouts.</p>
+            <p>Begin on your fitness journey today!</p>
+                <Link to="/workouts">
+                <button className="start-workout-button">Start Workout</button>
+                </Link>  
+        </div>        
+        
+
+        <div className="Pro-tips">
+            <h2>Pro Tips</h2>
+            <p>Here are some tips to help you get the most out of your workouts!</p>
         </div>
-    )
+    </div>
+    
+);
 }
 export default Home
